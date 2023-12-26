6a7d6e1114db6f800bfd229e3e0d4c48e07828e7
TK-Elliot <74335372+TK-Elliot@users.noreply.github.com>
2023-12-26 19:05:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const connection = mysql.createConnection({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set("view engine", "ejs");
+app.set('views', __dirname + '/views');
 
 app.listen(3000, () => {
 });
